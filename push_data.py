f3ddbd87061a56ee8f0e09d7c8c66761c76f5ef9
Omkar31415 <vullaganti.omkar.3@gmail.com>
2025-02-22 17:17:22 -0600

    Added Instructions and meaning of the working of code

--- push_data.py
+++ push_data.py
@@ -33,6 +33,14 @@ class NetworkDataExtract():
         try:
             data=pd.read_csv(file_path)
             data.reset_index(drop=True,inplace=True)
+            '''
+            data.T – Transposes the DataFrame (data), meaning rows become columns and columns become rows.
+            data.T.to_json() – Converts the transposed DataFrame into a JSON string.
+            json.loads(...) – Parses the JSON string into a Python dictionary.
+            .values() – Extracts the dictionary's values, which are typically dictionaries themselves (representing row-wise data after transposition).
+            list(...) – Converts the values (which are usually a dict_values object) into a standard Python list.
+            '''
+            # To know how "records" is working see the json_records.txt
             records= list(json.loads(data.T.to_json()).values())
             return records
         
@@ -41,14 +49,25 @@ class NetworkDataExtract():
 
     def insert_data_mongodb(self,records,database,collection):
         try:
+            '''
+            records: A list of dictionaries representing the data to be inserted.
+            database: The name of the MongoDB database.
+            collection: The name of the collection where data will be inserted.
+            '''
             self.database=database
             self.collection=collection
             self.records=records
 
+            #Creates a connection to the MongoDB server using the MONGO_DB_URL    
             self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            
+            #Accesses the specified database.
             self.database=self.mongo_client[self.database]
-
+            #Accesses the specified collection within that database.
             self.collection=self.database[self.collection]
+            
+            #Uses insert_many() to insert multiple records into the collection.
+            #Each record in self.records should be a dictionary representing a MongoDB document.
             self.collection.insert_many(self.records)
             return len(self.records)
         
@@ -61,7 +80,7 @@ class NetworkDataExtract():
 #     Collection='NetworkData'
 #     networkobj=NetworkDataExtract()
 #     records=networkobj.cv_to_json_converter(file_path=FILE_PATH)
-#     print(records)
+#     print(records[:1])
 #     no_of_records=networkobj.insert_data_mongodb(records,DATABASE,Collection)
 #     print(no_of_records)
 
